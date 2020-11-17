fb2e3a32d3077c067ff1e12d77b5e6437d1c1edd
Leandro Toledo <leandrotoledo@hotmail.com.br>
2020-11-17 00:52:20 +0000

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h2>Home 3</h2>
-            <Contador />
+            <h1>Home</h1>
         </div>
     )
 }
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-        return (
-            <div>
-                <div>{contador}</div>
-                <button onClick={adicionarContador}>Adicionar</button>
-            </div>
-        )
-    }
 
 export default Home
